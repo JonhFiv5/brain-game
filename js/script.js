9d37342fb4d9593817fabd78465ab548f8aac0c4
João Kaíque <joaok231@gmail.com>
2022-03-10 16:02:35 -0300

    Highlighting buttons according to sequence

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
-function hello() {
-    alert("Hello");
+function generateSequence(colors) {
+    let sequence = [];
+    for(let i = 0; i < 6; i++) {
+        sequence.push(Math.floor(Math.random() * colors.length));
+    }
+    return sequence;
+}
+
+function highlightButton(button) {
+    console.log(button.id);
+    
+    let defaultContrast = button.style.filter;
+    button.style.filter = "contrast(100%)";
+    
+    setTimeout(function() {
+        button.style.filter = defaultContrast;
+    }, 2000);
 }
